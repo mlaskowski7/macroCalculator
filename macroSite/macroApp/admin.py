87d7be9846f7ecb360c854d23df3a584f3f9b712
mlaskowski7 <mtlaskowski7@gmail.com>
2023-12-21 17:50:36 +0100

    setting up the template, index view

--- macroSite/macroApp/admin.py
+++ macroSite/macroApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+# superuser : username-admin, password-admin123
+admin.site.register(models.Food)
+admin.site.register(models.Consumption)
--- macroSite/macroApp/views.py
+++ macroSite/macroApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Food, Consumption
 
 # Create your views here.
+
+def index(request):
+
+    if request.method == 'POST':
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consumption = Consumption(user=user,food_consumed=consume)
+        consumption.save()
+    consumed_food = Consumption.objects.filter(user=request.user)
+    foodData = Food.objects.all()
+    context = {'foodData':foodData,'consumed_food':consumed_food}
+    return render(request,'index.html',context)
